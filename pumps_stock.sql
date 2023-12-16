6810a308db717a5c2213fabff1c4326cfcab3346
RetryR1v2 <147660115+RetryR1v2@users.noreply.github.com>
2023-12-16 16:55:07 +0100

    Delete pumps_stock.sql

--- pumps_stock.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `pumps_stock` (
-	`stash` TEXT NULL DEFAULT NULL COLLATE 'utf8_general_ci',
-	`item` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8_general_ci',
-	`oil` INT(11) NULL DEFAULT NULL
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB
-;
